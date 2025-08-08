7f911d09ef126811afcd5b4e39fe43ea7294d203
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-08-08 23:16:53 +0100

    interface implementation

--- interfaces/inter.go
+++ interfaces/inter.go
@@ -19,6 +19,48 @@ type Animal interface {
 }
 
 // Structs that will implement these interface
+type Dog struct {
+	Name string
+}
+
+type Cat struct {
+	Name string
+}
+
+type Robot struct {
+	Model string
+}
+
+// Dog implements speaker interface
+func (d Dog) Speak() string {
+	return fmt.Sprintf("%s says woof!", d.Name)
+}
+
+// Dog implements Mover interface
+func (d Dog) Move() string {
+	return fmt.Sprintf("%s runs around", d.Name)
+}
+
+// Cat implements speaker interface
+func (c Cat) Speak() string {
+	return  fmt.Sprintf("%s says meow!", c.Name)
+}
+
+// cat implements Mover interface
+func (c Cat) Move() string {
+	return fmt.Sprintf("%s prowls silently", c.Name)
+}
+
+// Robot implemets Speaker intertface
+func (r Robot) Speak() string {
+	return fmt.Sprintf("Robot %s says: BEEP BOOP ", r.Model)
+}
+
+// Robot implements mover interface
+func (r Robot) Move() string {
+	return  fmt.Sprintf("Robot %s moves mechanically", r.Model)
+}
+
 
 func main () {
 
