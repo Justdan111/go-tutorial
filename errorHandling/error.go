1a14c57ac895371fbb9bfb52c1ddd63cec296b43
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-08-11 18:22:31 +0100

    error wrapping and unwrapping

--- errorHandling/error.go
+++ errorHandling/error.go
@@ -33,6 +33,69 @@ func divide (a, b float64) (float64, error) {
 	return  a / b, nil
 }
 
+// Function with custom error
+func validateUser (u User) error {
+	if u.Name == " " {
+		return ValidationError{
+			Field: "name",
+			Message: "name cannot be empty",
+		}
+	}
+	if u.Age < 0 || u.Age > 150 {
+		return ValidationError{
+			Field: "age",
+			Message: "age must be between 0 and 150",
+		}
+	}
+	if len(u.Email) == 0 {
+		return  ValidationError{
+			Field: "email",
+			Message: "email is required",
+		}
+	}
+	return nil // No error
+}
+
+// Function that might return multiple types of errors
+func parseAndValidateAge(ageStr string) (int, error) {
+	// First, try to parse the string
+	age, err := strconv.Atoi(ageStr)
+	if err != nil {
+		return 0,
+	fmt.Errorf("failed to parse age '%s': %w", ageStr, err)	
+	}
+
+	// then validate the parsed age
+	if age < 0 {
+		return 0, errors.New("age cannot be negative")
+	}
+	if age > 150 {
+	   return 0, errors.New("age cannot be greater than 150")
+	}   
+	return age, nil
+}
+
+// demostration of error wrapping and unwrapping
+func processUserData(name, email, ageStr string) (*User, error) {
+	age, err := parseAndValidateAge(ageStr)
+	if err != nil {
+		return nil,
+		fmt.Errorf("error processing user data: %w", err)
+	}
+
+	User := User{
+		Name: name,
+		Email: email,
+		Age: age,
+	}
+
+	if err := validateUser(User);
+	err != nil {
+		return nil,
+		fmt.Errorf("user valodation failed: %w", err)
+	}
+	return &User, nil
+}
 
 func main() {
 
