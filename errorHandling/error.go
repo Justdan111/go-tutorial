08ffbb3f31ebfbb322aaf93f9b4472670f176aa4
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-08-13 22:31:56 +0100

    error

--- errorHandling/error.go
+++ errorHandling/error.go
@@ -115,4 +115,15 @@ func main() {
 	}
   
 	fmt.Println("\n === Custom Error Types ===")
+
+	// Valid User
+	validUser := User{Name: " ", Email: " ", Age: 25}
+	if err := validateUser(validUser); err != nil {
+		fmt.Printf("Validation error: %v\n", err)
+        
+        // Type assertion to get specific error details
+        if validationErr, ok := err.(ValidationError); ok {
+            fmt.Printf("Field with error: %s\n", validationErr.Field)
+        }
+
 }
