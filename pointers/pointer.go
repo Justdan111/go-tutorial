aa250b99b4f3ac4321d7b04f7adc84de24449997
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-08-15 23:07:21 +0100

    reciever

--- pointers/pointer.go
+++ pointers/pointer.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
+type Person struct {
+	Name string
+	Age  int
+}
+
+// function with value receiver
+func (p Person) IntroduceValue() {
+	fmt.Printf("Hi, I'm %s (value receiver)\n", p.Name)
+}
+
 
 func main() {
-	
+
 }
