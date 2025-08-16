c052c4d955520271cb68de26dbdedfdb1affbb4a
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-08-16 19:59:22 +0100

    pointer receiver

--- pointers/pointer.go
+++ pointers/pointer.go
@@ -12,6 +12,10 @@ func (p Person) IntroduceValue() {
 	fmt.Printf("Hi, I'm %s (value receiver)\n", p.Name)
 }
 
+// Method with pointer receiver - works with original
+func (p *Person) IntroducePointer() {
+    fmt.Printf("Hi, I'm %s (pointer receiver)\n", p.Name)
+}
 
 func main() {
 
