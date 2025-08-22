8327cf250c97b8722af862fee9f7f47647dd8698
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-08-22 14:44:24 +0100

    pass by value and pointer

--- miniProject/studentManagementSystem.go
+++ miniProject/studentManagementSystem.go
@@ -30,3 +30,30 @@ func ChangeNameValues(s Student, newName string) {
 func ChangeNamePointer(s *Student, newName string) {
 	s.Name = newName  // changes the original
 }
+
+func main() {
+	// 1 Basic Variables + pointers
+	x := 42
+	prt := &x
+	fmt.Println("Original x:", x)
+	*prt = 100
+	fmt.Println("Modified x through pointer:", x)
+
+	// 2 Structs and Pointers
+	student1 := Student("John", 20, "B")
+	studentPtr := &student1
+	fmt.Println("\nAccess via struct pointer:", studentPtr.Name)
+
+	// 3. Methods: value vs pointer receiver
+	student1.PrintInfo()
+	student1.UpdateGrade("A+")
+	student1.PrintInfo()
+
+	// 4 Functions: pass by value vs pointer
+	ChangeNameValues(student1, "Mike")
+	student1.PrintInfo() // Name should still be "John"
+
+	ChangeNamePointer(&student1, "Mike")
+	student1.PrintInfo() // Name should now be "Mike"
+	
+}
