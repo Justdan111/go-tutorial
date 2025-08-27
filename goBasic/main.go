3adf6a2f82b6da4083931adc56a511b33aa6a494
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-08-27 22:57:29 +0100

    Remove unused log import and clean up main function

--- goBasic/main.go
+++ goBasic/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
+	
 	"net/http"
 )
 
@@ -11,6 +11,5 @@ func main() {
 		fmt.Fprintf(w, "hello, world! You requested: %s", r.URL.Path)
 	})
 
-	fmt.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	
 }
