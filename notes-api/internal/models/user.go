c983d0e4a469200627c25fa39101ded91e10c335
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-08-30 20:45:16 +0100

    user

---
+++ notes-api/internal/models/user.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type User struct {
+	ID        string    `json:"id"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type SignupRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+	User  User  `json:"user"`
+}
+
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
