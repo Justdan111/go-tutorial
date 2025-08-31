be0469eccc72046a2010e0848cbeaef694a07492
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-08-31 19:51:18 +0100

    middleware

--- notes-api/internal/middleware/auth.go
+++ notes-api/internal/middleware/auth.go
@@ -1,1 +1,19 @@
 package middleware
+
+
+import (
+	"context"
+	"net/http"
+	"notes-api/internal/auth"
+	"strings"
+)
+
+type Authmiddleware struct {
+	jwtService *auth.JWTService
+}
+
+func NewAuthMiddleware(jwtService *auth.JWTService) *Authmiddleware {
+	return &Authmiddleware{
+		jwtService: jwtService,
+	}
+}
